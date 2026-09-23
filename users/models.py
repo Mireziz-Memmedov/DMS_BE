7aeaaf12a9c566384351b4c2dbafd15a059d765a
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-09-23 15:55:17 +0000

    commited

--- users/models.py
+++ users/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    class Meta:
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,13 @@
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from .models import User
 
 class LoginSerializer(TokenObtainPairSerializer):
     pass
+
+
+# class UserSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = User
+#         fields = "__all__"
