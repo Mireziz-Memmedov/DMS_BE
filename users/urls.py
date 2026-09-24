91c48bfbc45803d0a38043849fd4b53364e2b7d5
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-09-24 16:46:39 +0000

    commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+
 from . import views
 from .views import LoginView
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
+
     path("login/", LoginView.as_view(), name="login"),
+
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
     path("employees/", views.employees, name="employees"),
+
 ]
