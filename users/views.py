2716d703ab053ba0ec1426830910849cf21691ac
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-09-26 17:29:31 +0000

    commit

--- users/views.py
+++ users/views.py
@@ -84,9 +84,6 @@ def messages(request, conversation_id):
 @permission_classes([IsAuthenticated])
 def create_conversation(request):
 
-    print("LOGIN USER:", request.user.id, request.user.username)
-    print("REQUEST DATA:", request.data)
-    
     participant_ids = request.data.get(
         "participants",
         []
