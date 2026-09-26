bd9911d8f79ff81dc70166963914e20943601efa
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-09-26 15:32:38 +0000

    asgi.py

--- dms/asgi.py
+++ dms/asgi.py
@@ -1,16 +1,17 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from users.middleware import JWTAuthMiddleware
-from users.routing import websocket_urlpatterns
-
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "dms.settings"
 )
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from users.middleware import JWTAuthMiddleware
+from users.routing import websocket_urlpatterns
 
 
 django_asgi_app = get_asgi_application()
